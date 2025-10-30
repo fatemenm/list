8b80d665fb63e0e51af46336d1fac55cde5ca1b7
Fateme Noori Moghadam <fatemenm058@gmail.com>
2025-10-30 13:09:21 +0330

    fix: handle long subtitles by adding word break

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -13,11 +13,11 @@ export function ListItem({
 }) {
   return (
     <div className="flex flex-row justify-between p-4 bg-gray-50  rounded-md gap-10 shadow">
-      <div className="flex flex-col gap-1 grow">
+      <div className="flex flex-col gap-1 grow w-2/3">
         <h2 className="text-lg font-medium text-left">{item.title}</h2>
-        <div className="flex flex-row gap-4 items-center justify-between w-full text-sm">
-          <span>{item.subtitle}</span>
-          <span className="text-gray-400 text-xs">
+        <div className="flex flex-col gap-4  justify-between w-full text-sm">
+          <p className="wrap-break-word  text-left">{item.subtitle}</p>
+          <span className="text-gray-400 text-xs text-left shrink-0">
             {item.editedAt
               ? `edited at: ${item.editedAt.toString().split("GMT")[0]}`
               : `created at: ${item.createdAt.toString().split("GMT")[0]}`}
